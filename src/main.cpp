33a3b03f9e0479863bb86cea56969880b005b836
pikesley <github@orgraphone.org>
2017-01-18 17:23:00 +0000

    Outer ring works right
    Rewired everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,38 @@
 // Low power NeoPixel goggles example.  Makes a nice blinky display
 // with just a few LEDs on at any time.
 
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
-#define PIN 7
+#define NUM_LEDS 12
+#define DATA_PIN 7
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(21, PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  pixels.begin();
-  pixels.setBrightness(10); // 1/3 brightness
-  for(int j = 0; j < 19; j++) pixels.setPixelColor(j, 0);
-  pixels.show();
-  delay(100);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
+    FastLED.show();
+  }
 }
 
 void loop() {
-  pixels.setPixelColor(0, 0, 255, 0);
-  pixels.setPixelColor(1, 255, 0, 0);
-  pixels.setPixelColor(2, 0, 0, 255);
-  pixels.setPixelColor(4, 0, 255, 0);
-  pixels.setPixelColor(5, 255, 0, 0);
-  pixels.setPixelColor(6, 0, 0, 255);
-  pixels.setPixelColor(8, 0, 255, 0);
-
-  for (int i = 9; i < 21; i++) {
-    pixels.setPixelColor(i, 255, 0, 0);
+/*  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(50);
   }
 
-  pixels.show();
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Green;
+    FastLED.show();
+    delay(50);
+  }
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Blue;
+    FastLED.show();
+    delay(50);
+  }*/
 }
