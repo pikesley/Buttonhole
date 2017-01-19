abb13023fbb00c1f0ac640fe996cc139bcdad362
pikesley <github@orgraphone.org>
2017-01-19 11:02:31 +0000

    Named colours

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,58 @@
 
 #include <FastLED.h>
 
-#define NUM_LEDS 12
-#define DATA_PIN 7
+#define RING_LEDS 12
+#define RING_PIN 7
 
-CRGB leds[NUM_LEDS];
+int red[]     = {255,   0,   0};
+int orange[]  = {255, 127,   0};
+int yellow[]  = {255, 255,   0};
+int green[]   = {  0, 255,   0};
+int cyan[]    = {  0, 255, 255};
+int blue[]    = {  0,   0, 255};
+int magenta[] = {255,   0, 255};
 
-void setup() {
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+CRGB ring[RING_LEDS];
 
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Black;
+int speed() {
+  return 25;
+  //return random(500);
+}
+
+int interval() {
+  return 0;
+  //return random(500);
+}
+
+void wipe(int colour[]) {
+  for (int i = 0; i < RING_LEDS; i++) {
+    ring[i].r = colour[0];
+    ring[i].g = colour[1];
+    ring[i].b = colour[2];
     FastLED.show();
+    delay(speed());
   }
 }
 
+void setup() {
+  FastLED.addLeds<NEOPIXEL, RING_PIN>(ring, RING_LEDS);
+  FastLED.setCorrection(0x0f0f0f);
+  randomSeed(analogRead(0));
+}
+
 void loop() {
-/*  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(50);
-  }
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Green;
-    FastLED.show();
-    delay(50);
-  }
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Blue;
-    FastLED.show();
-    delay(50);
-  }*/
+  wipe(red);
+  delay(interval());
+  wipe(orange);
+  delay(interval());
+  wipe(yellow);
+  delay(interval());
+  wipe(green);
+  delay(interval());
+  wipe(cyan);
+  delay(interval());
+  wipe(blue);
+  delay(interval());
+  wipe(magenta);
+  delay(interval());
 }
